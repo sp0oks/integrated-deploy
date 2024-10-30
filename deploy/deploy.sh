59ad07e8067a3996c1b0d8e566ec4bddc103e9c8
Gabriel Alves <g4briel.4lves@gmail.com>
2024-10-30 11:28:33 -0300

    funcionou

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,17 +2,8 @@
 
 docker login --username ${GITHUB_USERNAME} --password ${GITHUB_TOKEN} ghcr.io
 
-# docker pull ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-api:${CIRCLE_SHA1:0:7}
-# docker pull ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-menu:${CIRCLE_SHA1:0:7}
-
-# docker stop cocktail-api
-# docker stop cocktail-menu
-
 docker compose down
 
 docker compose -f $1 up -d
-
-# docker run -p 8000:8000 --name cocktail-api --rm -d ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-api:${CIRCLE_SHA1:0:7} || exit 1
-# docker run -p 8080:8080 --name cocktail-menu --rm -d ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-menu:${CIRCLE_SHA1:0:7} || exit 1
 
 ufw allow 8080
