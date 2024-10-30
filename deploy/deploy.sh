3d043c575a1da0d844d0927eb7dca756ac9c5513
Gabriel Alves <g4briel.4lves@gmail.com>
2024-10-30 09:34:46 -0300

    atualiza url pra comunicação com a api

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,10 @@ docker login --username ${GITHUB_USERNAME} --password ${GITHUB_TOKEN} ghcr.io
 docker pull ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-api:${CIRCLE_SHA1:0:7}
 docker pull ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-menu:${CIRCLE_SHA1:0:7}
 
-docker run -p 8000:8000 -d ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-api:${CIRCLE_SHA1:0:7} || exit 1
-docker run -p 8080:8080 -d ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-menu:${CIRCLE_SHA1:0:7} || exit 1
+docker stop cocktail-api
+docker stop cocktail-menu
+
+docker run -p 8000:8000 --name cocktail-api -d ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-api:${CIRCLE_SHA1:0:7} || exit 1
+docker run -p 8080:8080 --name cocktail-menu -d ghcr.io/${GITHUB_USERNAME}/${CIRCLE_PROJECT_REPONAME}/cocktail-menu:${CIRCLE_SHA1:0:7} || exit 1
 
 ufw allow 8080
